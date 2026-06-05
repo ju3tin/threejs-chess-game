e5580c43269a82a0af40c863a2956067df8f9049
Justin <ju3tin@hotmail.co.uk>
2026-06-05 15:08:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} bg-background`}>
       <body className="font-sans antialiased">
         {children}
-        {process.env.NODE_ENV === 'production' && <Analytics />}
+     <Analytics />
       </body>
     </html>
   )
