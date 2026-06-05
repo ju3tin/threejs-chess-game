f81ee677d69f90e51aaad5f4272a73c0af1b4aaf
Justin <ju3tin@hotmail.co.uk>
2026-06-05 17:21:41 +0100

    Remove comments and clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add your config here (example below)
   reactStrictMode: true,
-  // experimental: { ... }  ← if you have any
 }
 
 module.exports = nextConfig
